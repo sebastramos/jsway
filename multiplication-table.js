a8869a3c625ca53bb2a3d290a113e74fa1aed3a7
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-04 16:56:42 +0300

    jsway exercice

--- multiplication-table.js
+++ multiplication-table.js
@@ -1,4 +1,26 @@
 // Multiplication table
 // Write a program that asks the user for a number, then shows the multiplication table for this number.
+// Write a program that asks the user for a number, 
+// then shows the multiplication table for this number.
 
 // When you are done, improve the program so it only accepts numbers between 2 and 9 (use the previous exercise as a blueprint).
+
+function askNumber() {
+	let question = Number(prompt('Input a number between 2 and 9'))
+  return question
+}
+
+function table() {  
+	let number = askNumber()
+  
+  if (number >= 2 && number <= 9) {
+  	for(let i = 1; i <= 9; ++i) {
+    	let result = i * number
+    	console.log(`${i} * ${number} = ${result}`)
+    }
+  } else {
+  	table()
+  }
+}
+
+table()
---
+++ neither-yes-nor-no.js
@@ -0,0 +1,3 @@
+// Write a program that plays "neither yes, nor no" with the user. 
+// Specifically, the program asks the user to enter text until either "yes" or "no" is typed, which ends the game.
+
