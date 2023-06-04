501f558c03be7fd9d7acf7b52aa41507ce588d70
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-04 19:33:26 +0300

    jsway exercise

---
+++ number-squaring.js
@@ -0,0 +1,16 @@
+// Square the given number x
+function square1(x) {
+  return  x * x
+}
+
+// Square the given number x
+const square2 = x => x * x
+
+
+console.log(square1(0)); // Must show 0
+console.log(square1(2)); // Must show 4
+console.log(square1(5)); // Must show 25
+
+console.log(square2(0)); // Must show 0
+console.log(square2(2)); // Must show 4
+console.log(square2(5)); // Must show 25
