35ba95ca91bc068c26d73b268e7eaba93cf17d52
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-04 17:05:25 +0300

    jsway exercice

--- neither-yes-nor-no.js
+++ neither-yes-nor-no.js
@@ -1,3 +1,7 @@
 // Write a program that plays "neither yes, nor no" with the user. 
 // Specifically, the program asks the user to enter text until either "yes" or "no" is typed, which ends the game.
 
+while (ask !== 'yes' && ask !== 'no') {
+	var ask = prompt('Enter "Yes" or "No"').toLowerCase()
+  console.log(`Your answer is ${ask}`)
+}
