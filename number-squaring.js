6a290749a1c6eacd6272b890e4d1867bdba5551c
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-04 19:59:13 +0300

    jsway exercise

--- number-squaring.js
+++ number-squaring.js
@@ -14,3 +14,14 @@ console.log(square1(5)); // Must show 25
 console.log(square2(0)); // Must show 0
 console.log(square2(2)); // Must show 4
 console.log(square2(5)); // Must show 25
+
+
+// When it's done, update the program so that it shows the square of every number between 0 and 10.
+
+function square() {
+  for(let i = 0; i <= 10; i++) {
+  		console.log(`This is the square of ${i} is ${i * i}`)
+	}
+}
+
+square()
